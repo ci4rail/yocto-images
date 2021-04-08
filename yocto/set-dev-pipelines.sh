cee3b9ae66d0516c2ff8100503969628b4831f6e
KlausPopp <klauspopp@gmx.de>
2021-04-08 16:39:50 +0200

    Update pipeline. Add testing pipeline to set-dev-pipelines.sh

--- yocto/set-dev-pipelines.sh
+++ yocto/set-dev-pipelines.sh
@@ -5,3 +5,4 @@ for image in cpu01-bringup cpu01-edgefarm verdindev-edgefarm; do
     fly -t dev unpause-pipeline -p ${image}-dev
 done
 
+fly -t dev set-pipeline -c pipeline_testing.yaml -p cpu01-edgefarm-acceptance-test -l ci/config-testing.yaml ci/config-dev.yaml -l ci/credentials.yaml -v name=cpu01-edgefarm

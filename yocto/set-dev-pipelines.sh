351830b773515f37e7a69cec5c14e5fa83274d87
KlausPopp <klauspopp@gmx.de>
2021-04-13 11:14:03 +0200

    fixed PR pipeline

--- yocto/set-dev-pipelines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for image in cpu01-bringup cpu01-edgefarm verdindev-edgefarm; do
-    fly -t dev set-pipeline -c pipeline.yaml -p ${image}-dev -l ci/config-dev.yaml -l ci/credentials.yaml -v name=${image}
-    fly -t dev unpause-pipeline -p ${image}-dev
-done
-
-fly -t dev set-pipeline -c pipeline_testing.yaml -p cpu01-edgefarm-acceptance-test -l ci/config-testing.yaml -l ci/config-dev.yaml -l ci/credentials.yaml -v name=cpu01-edgefarm

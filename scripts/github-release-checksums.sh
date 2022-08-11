be84ba2286626bd3410b274699b62349fb25a662
popples <klauspopp@gmx.de>
2022-08-11 15:25:13 +0200

    integrate socketcan-io4edge and ttynvt-runner (#59)
    * add github-release-checksums script
    * add Ttynvt and socketcan-io4edge runner

---
+++ scripts/github-release-checksums.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Get checksums for github release files
+# Generates snippet that can be pasted into .bb files
+#
+if [ "$#" -le 1 ] ; then
+    echo "Use ${0} <repo> <tag> <pattern>"
+    exit 1
+fi
+
+if ! [[ -x $(which gh) ]]; then
+    echo "gh tool does not exist. please download it"
+    exit 1
+fi
+
+repo="$1"
+tag="$2"
+
+pattern=""
+if [ "$#" -gt 2 ] ; then
+    pattern="-p $3"
+fi
+
+
+dldir=`mktemp -d`
+
+gh release download -D ${dldir} -R ${repo} ${tag} ${pattern}
+
+(
+    cd ${dldir}
+    for file in *.tar.gz; do
+        # extract arch from pathname
+        a=${file%.*.*} && arch=${a##*-};
+        md5sum=`md5sum $file | cut -f 1 -d ' '` 
+        sha256sum=`sha256sum $file | cut -f 1 -d ' '`
+        echo "SRC_URI[$arch.md5sum] = \"$md5sum\""
+        echo "SRC_URI[$arch.sha256sum] = \"$sha256sum\""
+    done
+
+)
+
+rm -rf ${dldir}
